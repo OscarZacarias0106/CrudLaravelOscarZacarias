8212ba9740a1f1546f63af0653917383d8bb4f9a
OscarZacarias0106 <ozacariaso@miumg.edu.gt>
2021-10-13 21:23:43 -0600

    Primer Proyecto CrudLaravel

---
+++ routes/web.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('usuarios.listar');
+});
+
+Route::get('/form','UserController@userform');
+Route::post('/save','UserController@save')->name('save');
